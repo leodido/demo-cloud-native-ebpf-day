06943ed2d935934ad1445e3c181caca6b2d3b252
Leonardo Di Donato <leodidonato@gmail.com>
2021-09-12 21:50:13 +0200

    update(src): print blocked IP with full-powered bpf_printk() if possible
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

--- src/restrict_connect.bpf.c
+++ src/restrict_connect.bpf.c
@@ -2,28 +2,47 @@
 #include <bpf/bpf_core_read.h>
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_tracing.h>
+#include "full_printk.bpf.h"
 
 char LICENSE[] SEC("license") = "GPL";
 
-const __u32 blockme = 1136196238; //16843009;
+#define EPERM 1
+#define AF_INET 2
+
+const __u32 blockme = 16843009; // 1.1.1.1 -> int
 
 SEC("lsm/socket_connect")
-int BPF_PROG(restrict_connect, struct socket *sock, struct sockaddr *address, int addrlen)
+int BPF_PROG(restrict_connect, struct socket *sock, struct sockaddr *address, int addrlen, int ret)
 {
+    // Exit if this is not the first LSM hook and the previous BPF program requires us to exit
+    if (ret != 0)
+    {
+        return ret;
+    }
+
     // Only IPv4 in this example
-    if (address->sa_family != 2 /* AF_INET */)
+    if (address->sa_family != AF_INET)
     {
         return 0;
     }
 
+    // Cast the address to an IPv4 socket address
     struct sockaddr_in *addr = (struct sockaddr_in *)address;
+
+    // Where do you want to go?
     __u32 dest = addr->sin_addr.s_addr;
 
-    bpf_printk("lsm: %d\n", dest);
     if (dest == blockme)
     {
-        bpf_printk("lsm: block\n");
-        return -1 /* -EPERM */;
+        if (full_printk)
+        {
+            bpf_printk("lsm: blocking %pI4h", &dest);
+        }
+        else
+        {
+            bpf_printk("lsm: blocking %d\n", dest);
+        }
+        return -EPERM;
     }
     return 0;
 }
